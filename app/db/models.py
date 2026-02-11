2380db98f25ffe64058003ec76e5c535cf162d61
amirabbs <amirabbasnzri8@gmail.com>
2026-02-11 20:14:06 +0330

    add ExpenseModel

--- app/db/models.py
+++ app/db/models.py
@@ -1,8 +1,7 @@
-from sqlalchemy import Column, Integer, String, Enum as SQLEnum
+from sqlalchemy import Column, Integer, String, Enum as SQLEnum, Float, DateTime, ForeignKey, func
+from sqlalchemy.orm import relationship
 from app.db.base import Base
 from enum import Enum
-
-
 
 class UserType(str, Enum):
     ADMIN = "admin"
@@ -15,10 +14,24 @@ class UserModel(Base):
     name = Column(String, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
-    role = Column(SQLEnum(UserType), nullable=False)
+    role = Column(SQLEnum(UserType), nullable=False, index=True)
     
+    expenses = relationship("ExpenseModel", back_populates="user")
     
     def __repr__(self):
         return f"<UserModel(id={self.id}, name='{self.name}', email='{self.email}', role='{self.role}')>"
+
+class ExpenseModel(Base):
+    __tablename__ = "expenses"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    amount = Column(Float, nullable=False)
+    description = Column(String, nullable=True)
+    created_at = Column(DateTime, default=func.now())
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    user = relationship("UserModel", back_populates="expenses", uselist=False)
     
-    
+    def __repr__(self):
+        return f"<ExpenseModel(id={self.id}, title='{self.title}', amount={self.amount}, user_id={self.user_id})>"
